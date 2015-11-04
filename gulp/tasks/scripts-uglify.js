59918eb736683c92c9786c4f253523d8b1229e11
Alain Schlesser <alain.schlesser@gmail.com>
2015-11-04 17:55:00 +0100

    gulp-changed messes with file creation, remove for now.

--- gulp/tasks/scripts-uglify.js
+++ gulp/tasks/scripts-uglify.js
@@ -17,11 +17,9 @@ gulp.task( 'scripts-uglify', function () {
 
 	var rename  = require( 'gulp-rename' );
 	var uglify  = require( 'gulp-uglify' );
-	var changed = require( 'gulp-changed' );
 	var config  = require( '../config' ).scripts;
 
-	return gulp.src( config.buildSrc )
-		.pipe( changed( config.dest ) )
+	return gulp.src( config.buildSrc, { base: config.base } )
 		.pipe( uglify() )
 		.pipe( rename( { extname: '.min.js' } ) )
 		.pipe( gulp.dest( config.dest ) );
